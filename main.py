08dcb00049038fa8245c954331569f2038b220de
ben <benjaminazj@gmail.com>
2022-11-27 04:10:33 +0800

    Day 28: Add Links, Subject, Topic selection, language, limit selection

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import requests
 from send_email import send_email
 
+
 user_email = "benjaminazj@gmail.com"
+topic = "tesla"
 
 api_key = "b7a4b4e8280e4e09b8147fe0ff92ed86"
-url = f"https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey={api_key}"
+url = "https://newsapi.org/v2/everything?"\
+    f"q={topic}&"\
+    "sortBy=publishedAt&"\
+    f"apiKey={api_key}"\
+    "&language=en"
 
 request = requests.get(url)
 contents = request.json()
 
 body = ""
-for content in contents['articles']:
+for content in contents['articles'][:20]:
     if content['title'] is not None:
-        body = body + content['title'] + '\n' + content['description'] + 2*'\n'
+        body = body + content['title']\
+               + '\n' + content['description'] + '\n'\
+               + content['url'] + 2*'\n'
+
 
 message = f"""Subject: News Letter
 
